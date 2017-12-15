3646cdf77584e4d62a3a6378ef56b169831ddc5f
Dilun7525 <freeworknet@yandex.ru>
2017-12-15 20:28:23 +0700

    Create function decompositionArray($objParam) - разбивает ассоциативный массив на 2 массива: ключей и значений. Возвращиется масив масивов;

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <?php
 /*
  * получаем начальные настройки кодировок,
- * переменные с путями скриптов ($pathRoot,$pathBD)
+ * переменные с путями скриптов ($pathRoot,$pathBD, $pathConfigs)
  * */
-require_once $_SERVER['DOCUMENT_ROOT'] . '/protected/m/initial_setup/setup.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/protected/configs/initial_setup/setup.php';
 
-require ($pathRoot.'protected/m/bd/bd.php');
+require($pathRoot . 'protected/m/db/db.php');
 
-$bd=new bd($pathBD.'bd_install.txt');
-$bd->connect();
-$bd->disconnect();
+$dataBase1 = db::getInstance($pathConfigs . 'db_install.txt');
+$dataBase1->connect();
+$object=[
+	'test1'=>"O'kipling<rger>\||",
+	'test2'=>"boergergeok's"
+];
+	$where='id=1';
+$dataBase1->update('test_table',$object,$where);
 
-$bd=new bd($pathBD.'bd_install.txt');
-$bd->connect();
-$bd->disconnect();
 
-$bd=new bd($pathBD.'bd_install.txt');
-$bd->connect();
-$bd->disconnect();
+
+$dataBase1->disconnect();
+
+
+$dataBase2 = db::getInstance($pathConfigs . 'db_install.txt');
+$dataBase2->connect();
+$dataBase2->disconnect();
 
 //Вызываем контроллер
 //require ($pathRoot.'protected/c/c_common.php');
+
+
+
+
