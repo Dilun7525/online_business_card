b9276ac475d48701a34dfad640e01f2cf9e06497
Dilun7525 <freeworknet@yandex.ru>
2017-12-25 09:55:37 +0700

    header:: создана структура: логотип, фото пользователя или пустой значек, смена названия на кнопке авторизации

--- protected/v/template/header.php
+++ protected/v/template/header.php
@@ -1,25 +1,57 @@
-<!--
-Шаблон страницы с подключением CSS.
-Необходима переменная $pathRoot
-для определения корня сайта
--->
+<?php
+/*
+ * получаем начальные настройки сайта
+ * */
+require_once $_SERVER['DOCUMENT_ROOT'] .
+	'protected/configs/initial_setup/setup.php';
+$title = "Home";
+$logo = "/protected/m/img/logo.png";
+$authorization = true;
+$login = "Vasiy";
+$fotoUser="/protected/m/img/22.jpg";
+?>
 
 
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Required meta tags -->
+	<meta charset="utf8mb4">
+	<meta name="viewport"
+		  content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>PHP Уровень 2</title>
+	<title><?= $title ?></title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-          integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="<?php echo $pathRoot; ?>css/style.css">
-
-
+	<!-- CSS -->
+	<link rel="stylesheet"
+		  href="/css/bootstrap.min.css"
+	>
+	<link rel="stylesheet" href="/css/style.css">
 </head>
-<body>
+<body class="cont-bg">
+<header class="container">
+	<div class="row">
+		<div class="col-6 ">
+			<div class="row justify-content-start align">
+				<a href="/"><img  src="<?= $logo ?>" alt="logo"></a>
+			</div>
+
+		</div>
+		<div class="col-6">
+			<div class="row justify-content-end align-items-center">
+				<?php if (!$authorization): ?>
+					<img   src="/protected/m/img/user.png"
+						 alt="logo">
+					<button type="button" class="btn
+				btn-outline-dark">Регистрация</button>
+				<?php else: ?>
+					<img  class="user-img" src="<?=$fotoUser?>" alt="">
+					<button type="button" class="btn btn-outline-success"><?= $login?></button>
+					<a href="/"><img  src="/protected/m/img/user_exit.png" alt="logo"></a>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</header>
+
+
