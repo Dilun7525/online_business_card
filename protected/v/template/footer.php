64f24eef47aacf34ed34805cc4442b01d4aab703
Dilun7525 <freeworknet@yandex.ru>
2017-12-26 12:51:34 +0700

    Heder & Footer - добавлен функционал адаптивности

--- protected/v/template/footer.php
+++ protected/v/template/footer.php
@@ -5,29 +5,24 @@
 для определения корня сайта
 -->
 
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="/protected/m/js/bootstrap.min.js"></script>
-<script src="/protected/m/js/bootstrap.bundle.min.js"></script>
-<script src="/protected/m/js/popper.js"></script>
 
 
 <footer class="container ">
 
-	<div class="row align-center">
-		<div class="col-4 ">
+	<div class="row  align-items-center no-gutters">
+		<div class="col-6 col-sm-4 order-1">
 			<div class="row justify-content-start">
 				<a href="/"><img src="<?= $logo ?>" alt="logo"></a>
 			</div>
 		</div>
-		<div class="col">
-			<div class="row justify-content-center">
+		<div class="col order-4 order-sm-2">
+			<div class="row justify-content-sm-center">
 				<img src="/protected/m/img/logo_copyright.png" alt="Copyright">
 				Anatoly Lazarev
 			</div>
 		</div>
-		<div class="col-4 ">
-			<div class="row justify-content-end">
+		<div class="col-6 col-sm-4 order-3">
+			<div class="row justify-content-end align-items-center">
 				<a href="mailto:freeworknet@yandex.ru">
 					<img src="/protected/m/img/logo_email.png" alt="email"></a>
 				<a href="skype:Dilun13?add">
@@ -39,6 +34,12 @@
 		</div>
 	</div>
 </footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/protected/m/js/bootstrap.js"></script>
+<script src="/protected/m/js/bootstrap.bundle.js"></script>
+<script src="/protected/m/js/popper.js"></script>
 
 </body>
 </html>
--- protected/v/template/header.php
+++ protected/v/template/header.php
@@ -7,8 +7,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] .
 $title = "Home";
 $logo = "/protected/m/img/logo.png";
 $authorization = true;
-$login = "Vasiy";
-$fotoUser="/protected/m/img/22.jpg";
+$login = "Kuty";
+$fotoUser = "/protected/m/img/22.jpg";
 ?>
 
 
@@ -16,38 +16,43 @@ $fotoUser="/protected/m/img/22.jpg";
 <html lang="ru">
 <head>
 	<!-- Required meta tags -->
-	<meta charset="utf8mb4">
+	<meta charset="utf8">
 	<meta name="viewport"
 		  content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<title><?= $title ?></title>
 
 	<!-- CSS -->
-	<link rel="stylesheet"
-		  href="/css/bootstrap.min.css"
-	>
+	<!--<link rel="stylesheet"
+		  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+		  integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+		  crossorigin="anonymous">-->
 	<link rel="stylesheet" href="/css/style.css">
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+
 </head>
 <body class="cont-bg">
 <header class="container">
-	<div class="row">
-		<div class="col-6 ">
-			<div class="row justify-content-start align">
-				<a href="/"><img  src="<?= $logo ?>" alt="logo"></a>
+	<div class="row  align-items-center no-gutters">
+		<div class="col-6">
+			<div class="row justify-content-start">
+				<a href="/"><img src="<?= $logo ?>" alt="logo"></a>
 			</div>
-
 		</div>
 		<div class="col-6">
 			<div class="row justify-content-end align-items-center">
 				<?php if (!$authorization): ?>
-					<img   src="/protected/m/img/user.png"
+					<img src="/protected/m/img/user.png"
 						 alt="logo">
 					<button type="button" class="btn
-				btn-outline-dark">Регистрация</button>
+				btn-outline-dark">Регистрация
+					</button>
 				<?php else: ?>
-					<img  class="user-img" src="<?=$fotoUser?>" alt="">
-					<button type="button" class="btn btn-outline-success"><?= $login?></button>
-					<a href="/"><img  src="/protected/m/img/user_exit.png" alt="logo"></a>
+					<img class="user-img" src="<?= $fotoUser ?>" alt="">
+					<button type="button"
+							class="btn btn-outline-success"><?= $login ?></button>
+					<a href="/"><img src="/protected/m/img/user_exit.png"
+									 alt="logo"></a>
 				<?php endif; ?>
 			</div>
 		</div>
