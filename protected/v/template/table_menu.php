70b3067989d236f30ac3130c7bf6d6b96d86bb1c
Dilun7525 <freeworknet@yandex.ru>
2017-12-27 11:18:05 +0700

    создан макет меню в виде графической таблицы

---
+++ protected/v/template/table_menu.php
@@ -0,0 +1,11 @@
+<!--<div class="container">
+	<div class="row">-->
+		<div class="col-12 col-sm-6 col-md-4">
+			<div class="row justify-content-center">
+				<div class="focus pic">
+					<img class="img-size" src="/protected/m/img/7.jpg"
+						 alt="beach">
+				</div>
+			</div>
+<!--	</div>
+</div>-->
