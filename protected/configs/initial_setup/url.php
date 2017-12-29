14e220a1c41f0928eb60c81e4668353f5297e0b0
Dilun7525 <freeworknet@yandex.ru>
2017-12-29 19:30:00 +0700

    Переменные путей переделал в константы

--- protected/configs/initial_setup/url.php
+++ protected/configs/initial_setup/url.php
@@ -1,11 +1,22 @@
 <?php
 /**Возврашает пути скриптов, используемых в проекте
- * @return $pathRoot 	- до дириктории проекта
- * @return $pathBD 	- до дириктории базы данных
- * @return $pathConfigs- до дириктории конфигурационных файлов
+ * @return "PATH_ROOT" 		- проекта
+ * @return "PATH_CONFIGS"	- конфигурационных файлов
+ * @return "PATH_MODEL"		- модели
+ * @return "PATH_VIEW"		- шаблонов
+ * @return "PATH_CONTROLLER"- контролеров
+ * @return "PATH_BD" 		- базы данных
+ * @return "PATH_IMG"		- изображений
+ * @return "PATH_BD_IMG"	- изображений для работы с БД
  * */
-$pathRoot 	= $_SERVER['DOCUMENT_ROOT'];
-$pathBD 	= $_SERVER['DOCUMENT_ROOT'] . 'protected/m/db/';
-$pathConfigs= $_SERVER['DOCUMENT_ROOT'] . 'protected/configs/';
-$pathImg	= $_SERVER['DOCUMENT_ROOT'] . 'protected/m/img/';
+define("PATH_ROOT",		 	$_SERVER['DOCUMENT_ROOT']);
+define("PATH_CONFIGS",	 	'protected/configs/');
+define("PATH_MODEL",	 	"protected/m/");
+define("PATH_VIEW", 	 	"protected/v/");
+define("PATH_CONTROLLER",	"protected/c/");
+define("PATH_BD",		 	"protected/m/db/");
+define("PATH_BD_IMG",	 	"protected/m/db/img/");
+define("PATH_IMG",	 	 	"protected/m/img/");
+
+
 
