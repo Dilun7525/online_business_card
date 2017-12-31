fbc228072a8cee5711428c22e373664ac59f88b6
Dilun7525 <freeworknet@yandex.ru>
2017-12-31 21:09:46 +0700

    Создана опорная точка сайта: index.php

--- index.php
+++ index.php
@@ -5,14 +5,39 @@
  * */
 require_once $_SERVER['DOCUMENT_ROOT'] .
 	'protected/configs/initial_setup/setup.php';
+session_start();
 
-require($pathRoot . 'protected/v/template/header.php');
-require($pathRoot . 'protected/v/template/footer.php');
+// Разбираем URL на параметры, 1-й контроллер, 2-й action
+// например /articles/edit
+$urlinfo = $_SERVER['REQUEST_URI'];
+$urlparts = explode('/', $urlinfo);
 
+$params = [];
 
+foreach ($urlparts as $v) {
+	if ($v != 'index.php' && $v != '') {
+		$params[] = $v;
+	}
+}
 
-//Вызываем контроллер
-//require ($pathRoot.'protected/c/c_common.php');
+// извлекаем из массива параметров контроллер
+$controller = isset($params[0]) ? array_shift($params) : 'index';
+
+// извлекаем из массива параметров action
+$action = isset($params[0]) ? array_shift($params) : 'index';
+
+// Динамически формируем контроллер
+$controller = 'C_' . ucfirst($controller);
+// Формируем action
+$action = 'action_' . $action;
+
+// Контроллер по-умолчанию
+if (!class_exists($controller)) {
+	$controller = 'C_Home';
+}
+
+$controllerObj = new $controller();
+$controllerObj->Request($action, $params);
 
 
 
