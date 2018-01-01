e18763ba9bf595532179e002322405c170afa975
Dilun7525 <freeworknet@yandex.ru>
2018-01-01 15:25:33 +0700

    изминения пути изображений БД

--- protected/v/template/header.php
+++ protected/v/template/header.php
@@ -1,18 +1,7 @@
 <?php
-/*
- * получаем начальные настройки сайта
- * */
-/*require_once $_SERVER['DOCUMENT_ROOT'] .
-	'protected/configs/initial_setup/setup.php';*/
-require_once PATH_ROOT.PATH_CONFIGS."initial_setup/setup.php";
-$title = "Home";
-$logo = "logo.png";
-$authorization = true;
-$login = "Kuty";
-$fotoUser = "7.jpg";
-$imgMenu  ="7.jpg";
-$descriptionImgMenu="Розняшка";
-
+/**получаем начальные настройки сайта
+ */
+require_once PATH_ROOT . PATH_CONFIGS . "initial_setup/setup.php";
 ?>
 
 
@@ -40,19 +29,19 @@ $descriptionImgMenu="Розняшка";
 	<div class="row  align-items-center no-gutters">
 		<div class="col-6">
 			<div class="row justify-content-start">
-				<a href="/"><img src="<?= PATH_IMG.$logo ?>" alt="logo"></a>
+				<a href="/"><img src="<?= PATH_IMG . $logo ?>" alt="logo"></a>
 			</div>
 		</div>
 		<div class="col-6">
 			<div class="row justify-content-end align-items-center">
 				<?php if (!$authorization): ?>
-					<img src="/protected/m/img/user.png"
-						 alt="logo">
+					<img src="/protected/m/img/user.png" alt="logo">
 					<button type="button" class="btn
 				btn-outline-dark">Регистрация
 					</button>
 				<?php else: ?>
-					<img class="user-img" src="<?= PATH_BD_IMG.$fotoUser ?>" alt="">
+					<img class="user-img" src="<?= PATH_DB_IMG . $fotoUser ?>"
+						 alt="">
 					<button type="button"
 							class="btn btn-outline-success"><?= $login ?></button>
 					<a href="/"><img src="/protected/m/img/user_exit.png"
@@ -66,5 +55,4 @@ $descriptionImgMenu="Розняшка";
 <!--Content-->
 <div class="container">
 	<div class="row">
-		<?php $content?>
 
