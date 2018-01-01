672e4f318c0d02187d74f3cc8e78b922f4898215
Dilun7525 <freeworknet@yandex.ru>
2018-01-01 21:46:39 +0700

    Поправил шаблон и пути к изображениям

--- protected/v/template/table_menu.php
+++ protected/v/template/table_menu.php
@@ -1,8 +1,17 @@
+<?php
+/**
+ * @param $linkimgMenu			- ссылка на другую страницу
+ * @param $imgMenu				- имя файла с изображением
+ * @param $descriptionImgMenu	- описание изображения
+ */
+?>
 <div class="col-12 col-sm-6 col-md-4">
 	<div class="row justify-content-center">
 		<div class="focus pic">
-			<img class="img-size" src="<?=PATH_BD_IMG.$imgMenu?>"
-				 alt="<?=$descriptionImgMenu?>">
+			<a href="<?= $linkimgMenu?>">
+				<img class="img-size" src="/<?=PATH_DB_IMG.$imgFile?>"
+					 alt="<?=$descriptionImgMenu?>">
+			</a>
 		</div>
 	</div>
 </div>
