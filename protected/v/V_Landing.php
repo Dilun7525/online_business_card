d73a481a6d59538ac0b59fe4ef85bbae4b20cc0b
Dilun7525 <freeworknet@yandex.ru>
2018-01-09 20:19:07 +0700

    Шаблон лендинга из 3 блоков и min css к ним - будет отработка JS

---
+++ protected/v/V_Landing.php
@@ -0,0 +1,15 @@
+<?php
+/**загрузка заголовка*/
+require_once $_SERVER['DOCUMENT_ROOT'] . 'protected/v/template/setup_head.php';?>
+<!--// todo  Сначала три блока, для отработки JS.-->
+<body>
+<!--Content-->
+<div class="container-fluid ">
+	<div class="card-deck">
+		<div class="layer card1"></div>
+		<div class="layer card2"></div>
+		<div class="layer card3"></div>
+	</div>
+</div>
+</body>
+</html>
